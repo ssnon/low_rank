f7ca1e58f703c09e6f674ff8e6210b12869d2067
supelee13579 <leebskhan@gmail.com>
2024-01-13 09:59:43 +0900

    lora_head update

--- examples/NLU/custum_roberta_base_mnli.sh
+++ examples/NLU/custum_roberta_base_mnli.sh
@@ -1,26 +1,25 @@
-export num_gpus=8
 export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
 export PYTHONHASHSEED=0
-export output_dir="./mnli"
-python -m torch.distributed.launch --nproc_per_node=$num_gpus \
-examples/text-classification/run_glue.py \
+export output_dir="./output"
+python examples/text-classification/run_glue_head.py \
 --model_name_or_path roberta-base \
 --task_name mnli \
 --do_train \
 --do_eval \
 --max_seq_length 512 \
---per_device_train_batch_size 16 \
+--per_device_train_batch_size 8 \
 --learning_rate 5e-4 \
---num_train_epochs 30 \
---output_dir $output_dir/model \
+--num_train_epochs 15 \
+--output_dir $output_dir/headwiseB_mnli_roberta \
 --overwrite_output_dir \
 --logging_steps 10 \
---logging_dir $output_dir/log \
+--logging_dir $output_dir/headwiseB_mnli_roberta/log \
 --evaluation_strategy epoch \
 --save_strategy epoch \
 --warmup_ratio 0.06 \
 --apply_lora \
---lora_r 8 \
+--lora_r 1 \
 --lora_alpha 16 \
 --seed 0 \
---weight_decay 0.1
+--weight_decay 0.01 \
+--ex_type lora_headwiseB
